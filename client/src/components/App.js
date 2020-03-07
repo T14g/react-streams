ed4e6ac5d9f7eefa8d1c8deb802b43de0f9e552f
t14g <ottiagos@gmail.com>
2020-03-07 19:39:52 -0300

    Router w/ Link

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 
-// React Router DOM exemple
+//React Router DOM exemple
 //Type http://localhost:3000/pagetwo
+//Exact == Exact URL, else it looks if the url Contains the path
+//Router prevents reloading by browser
+
 
 const PageOne = () =>{
-    return <div>Page One!</div>
+    return (
+        <div>
+            Page One!
+            <Link to="/pagetwo">Page 2</Link>
+        </div>
+    );
 };
 
 const PageTwo = () =>{
-    return <div>Page Two!</div>
+    return (
+        <div>
+            Page Two!
+            <Link to="/">Page 1</Link>
+        </div>
+    );
 };
 
 

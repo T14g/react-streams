55b7199b4081307c86dabb571edb20578f781e4a
t14g <ottiagos@gmail.com>
2020-03-23 03:09:12 -0300

    Programmatic Navigation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 
 import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
@@ -7,6 +7,7 @@ import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import history from '../history';
 
 //Exact == Exact URL, else it looks if the url Contains the path
 //Router prevents reloading by browser 
@@ -14,7 +15,7 @@ import StreamShow from './streams/StreamShow';
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
             <Header/>
                 <div>
                     <Route path="/" exact component={StreamList} />
@@ -23,7 +24,7 @@ const App = () => {
                     <Route path="/streams/delete" exact component={StreamDelete} />
                     <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 };
